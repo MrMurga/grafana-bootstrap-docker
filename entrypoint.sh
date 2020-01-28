4562380d678ebc56e4f1fb050891c01267d7e324
Armando Murga <armando.murga@gusto.com>
2020-01-27 22:13:41 -0800

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+
+GF_API=${GF_API:-http://desolate-brook-68995.herokuapp.com}
+GF_USER=${GF_USER:-admin}
+GF_PASSWORD=${GF_PASSWORD:-admin}
+
+BACKEND=${BACKEND:-graphite}
+
+print_header() {
+  echo " "
+  echo "------------------"
+  echo $1
+  echo "------------------"
+}
+
+print_header "Done!"
